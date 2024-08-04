533e573b2ade320443adda7bab0b04bd5d7c65d6
namaek2 <science4588@gmail.com>
2024-08-04 22:00:54 +0900

    debugging print added

--- src/main/resources/pyscripts/stringObfuscate.py
+++ src/main/resources/pyscripts/stringObfuscate.py
@@ -8,9 +8,10 @@ from obfuscateTool import obfuscateTool
 class StringObfuscate:
     def __init__(self, output_folder, keyDecryptJava, stringDecryptJava):
         searched_strings = stringSearch(output_folder)
-
+        print("string search complete")
         encrypted_strings = stringEncrypt(searched_strings.Literals)
-
+        print("string encrypt complete")
         random_classes = obfuscateTool.random_class(searched_strings.class_names, 2)
         stringInsert(searched_strings.Literals, encrypted_strings.encrypted_Literals, random_classes, output_folder,
                      keyDecryptJava, stringDecryptJava)
+        print("string insert complete")
