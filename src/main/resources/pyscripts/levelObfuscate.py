3e46eebb97f2a2722bca57e5473dd1f034bf0f79
namaek2 <science4588@gmail.com>
2024-09-01 21:09:24 +0900

    dummy finished

--- src/main/resources/pyscripts/levelObfuscate.py
+++ src/main/resources/pyscripts/levelObfuscate.py
@@ -1,6 +1,7 @@
-from operationExtract import ExtractOperations
 from operationObfuscate import ObfuscateOperations
 from applyObfuscated import ApplyObfuscated
+from dumbDB import DumbDB
+from dummyInsert import InsertDummyCode
 
 import json
 
@@ -11,12 +12,10 @@ class LevelObfuscation:
 
         self.check_level(tainted_json)
 
-
     def parse_json(self, json_file_path):
         with open(json_file_path, 'r', encoding='utf-8') as file:
             ob_json = json.load(file)
         return ob_json
-
 
     def check_level(self, json):
         for item in json:
@@ -26,13 +25,27 @@ class LevelObfuscation:
                 continue
 
             if item["sensitivity"] == 3:
-                continue
-                # for tainted in item["tatinted"]:
-                #     print("  File Path:", tainted["file_path"])
-                #     print("  Method Name:", tainted["method_name"])
-                #     print("  Tree Position:", tainted["tree_position"])
-                #     print("  Source Code:", tainted["source_code"])
-                #     print("  -" * 50)
+                ddb = DumbDB()
+                for tainted in item["tainted"]:
+                    # 연산자 난독화
+                    print("연산자")
+                    O = ObfuscateOperations(tainted)
+                    obfuscated_code = O.return_obfuscated_code()
+
+                    # 더미 코드 추가
+                    rand = ddb.get_unique_random_number()
+                    if rand is not None:
+                        print("더미코드")
+                        dummy_code = ddb.get_dumb(rand)
+                        idc = InsertDummyCode(obfuscated_code, dummy_code, rand)
+                        if idc.get_obfuscated_code() is not None:
+                            obfuscated_code = idc.get_obfuscated_code()
+
+                    else:
+                        continue
+
+                    print(obfuscated_code)
+                    ApplyObfuscated(tainted["file_path"], tainted["source_code"], obfuscated_code)
 
             if item["sensitivity"] == 2:
                 for tainted in item["tainted"]:
