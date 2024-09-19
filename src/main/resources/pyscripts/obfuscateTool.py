f2f96968429890650d3ca05ebd90a77f128b4b69
namaek2 <science4588@gmail.com>
2024-09-20 04:33:58 +0900

    key obfuscation fixed

--- src/main/resources/pyscripts/obfuscateTool.py
+++ src/main/resources/pyscripts/obfuscateTool.py
@@ -6,7 +6,7 @@ import os
 class ObfuscateTool:
     def random_class(class_list, random_count):
         leng = len(class_list)
-        #random_indices = [random.randint(0, leng - 1) for _ in range(random_count)] #스패로우
+
         random_indices = [secrets.randbelow(leng) for _ in range(random_count)]
 
         random_class = [class_list[i] for i in random_indices]
@@ -29,6 +29,12 @@ class ObfuscateTool:
                     try:
                         tree = javalang.parse.parse(source_code)
                         java_files.append((file_path, tree, source_code))
-                    except Exception as e:
+
+                    except javalang.parser.JavaParserError as e:
                         print(f"Error parsing file {file_path}: {e}")
+                    except javalang.parser.JavaSyntaxError as e:
+                        print(f"Syntax error in file {file_path}: {e}")
+
+
+                        java_files.append((file_path, tree, source_code))
         return java_files
