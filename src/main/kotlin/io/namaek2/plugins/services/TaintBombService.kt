bbc38c8005d6df3b7b23add4627f10a63f85bc12
namaek2 <science4588@gmail.com>
2024-09-20 11:07:02 +0900

    plugin refactored

---
+++ src/main/kotlin/io/namaek2/plugins/services/TaintBombService.kt
@@ -0,0 +1,39 @@
+package io.namaek2.plugins.services
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
+import io.namaek2.plugins.MyBundle
+import io.namaek2.plugins.toolWindow.MyConsoleLogger
+
+
+@Service(Service.Level.PROJECT)
+class TaintBombService(private val project: Project) {
+    private val projectFolder = project.basePath
+
+    init {
+        thisLogger().info(MyBundle.message("projectService", project.name))
+    }
+
+    fun startTaintBomb() {
+        val javaFilesPath = projectFolder
+        val outFolder = "$projectFolder/obfuscated_project_folder"
+        val tempFolder = "$projectFolder/temp"
+        MyConsoleLogger.clearConsole()
+
+        if (javaFilesPath != null) {
+            ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Running tasks...") {
+                override fun run(indicator: ProgressIndicator) {
+                    indicator.isIndeterminate = false
+
+                    TasksManager(javaFilesPath, outFolder, tempFolder, indicator)
+                }
+            })
+        } else {
+            thisLogger().error("Project path is null")
+        }
+    }
+}
