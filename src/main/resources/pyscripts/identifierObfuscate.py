172f1a4dd6271be531dbc78208e6e73fe4a236bd
TheMac223 <wnsgud5235@naver.com>
2024-09-29 17:01:22 +0900

    fixfix

--- src/main/resources/pyscripts/identifierObfuscate.py
+++ src/main/resources/pyscripts/identifierObfuscate.py
@@ -1,17 +1,17 @@
 import os
 import javalang
-import secrets
+import random
 import re
 
 class ob_identifier:
+
     def __init__(self, folder_path, output_folder):
         self.folder_path = folder_path
         self.output_folder = output_folder
         self.identifier_map = {}  # 난독화 맵
         self.files = []  # 파일 경로 저장
         self.package_map = []  # 패키지 이름 저장 or set으로 해야할지도
-
-        self.ran = secrets.choice([1, 2, 3])
+        self.ran = random.choice([1, 2, 3])
 
         # 파일 수집 및 난독화 맵 구성
         self.collect_files()
@@ -21,6 +21,8 @@ class ob_identifier:
         self.apply_obfuscation_to_files()
         print(self.identifier_map)
 
+
+
     def generate_obfuscated_name(self, name, length=8):
         """난독화된 이름을 생성합니다."""
         if name not in self.identifier_map and name != 'main':
@@ -29,15 +31,17 @@ class ob_identifier:
 
             while True:
                 if ran == 1:
-                    obfuscated_name = ''.join([secrets.choice(["l", "I"])]) + ''.join([secrets.choice(['l', '1', 'I']) for _ in range(length)])
+                    obfuscated_name = ''.join(random.choices(["l", "I"], k=1)) + ''.join(random.choices(['l', '1', 'I'], k=length))
                 elif ran == 2:
-                    obfuscated_name = ''.join([secrets.choice(['l', 'I', 'α', 'β', 'γ', 'δ', 'π'])]) + ''.join([secrets.choice(['l', '1', 'I', 'α', 'β', 'γ', 'δ', 'π']) for _ in range(length)])
+                    obfuscated_name = ''.join(random.choices(['l', 'I', 'α', 'β', 'γ', 'δ', 'π'], k=1)) + ''.join(random.choices(['l', '1', 'I', 'α', 'β', 'γ', 'δ', 'π'], k=length))
                 elif ran == 3:
-                    obfuscated_name = ''.join([secrets.choice(['O', 'o'])]) + ''.join([secrets.choice(['0', 'O', 'o', 'Ο', 'о']) for _ in range(length)])
+                    obfuscated_name = ''.join(random.choices(['O', 'o'], k=1)) + ''.join(random.choices(['0', 'O', 'o', 'Ο', 'о'], k=length))
 
                 if obfuscated_name not in self.identifier_map.values():
-                    self.identifier_map[name] = obfuscated_name                    
+                    self.identifier_map[name] = obfuscated_name
                     break
+
+
 
     def collect_files(self):
         """모든 자바 파일을 수집합니다."""
@@ -46,6 +50,9 @@ class ob_identifier:
                 if file.endswith('.java'):
                     file_path = os.path.join(root, file)
                     self.files.append(file_path)
+
+
+
 
     def build_obfuscation_map(self):
         """모든 파일을 처리하고 난독화할 식별자를 수집하여 맵을 구성합니다."""
@@ -59,14 +66,19 @@ class ob_identifier:
             # AST에서 식별자 수집 및 난독화 맵 구축
             self.collect_identifiers_from_ast(tree, file_path)
 
+
+
     def collect_identifiers_from_ast(self, tree, file_path):
-        for path, node in tree:
-            # 패키지 선언
+        current_class = None
+
+        for path, node in tree: # 패키지 선언
             if isinstance(node, javalang.tree.PackageDeclaration):
                 self.package_map.append(node.name)  # 패키지 이름 저장
 
+
             # 클래스나 Enum 선언
             elif isinstance(node, javalang.tree.ClassDeclaration) or isinstance(node, javalang.tree.EnumDeclaration):
+                current_class = node.name
                 self.generate_obfuscated_name(node.name)
 
             # 메서드 선언
@@ -83,8 +95,8 @@ class ob_identifier:
 
             # 객체 생성 및 변수 선언
             elif isinstance(node, javalang.tree.LocalVariableDeclaration):
-                # 변수 선언에서 객체가 생성될 때의 변수 이름 처리
-                for declarator in node.declarators:
+                for declarator in node.declarators: # 변수 이름
+
                     self.generate_obfuscated_name(declarator.name)
 
             # Try 문과 그 안에 있는 리소스, 변수, 메서드 호출 처리
@@ -99,12 +111,13 @@ class ob_identifier:
                     if isinstance(statement, javalang.tree.LocalVariableDeclaration):
                         for declarator in statement.declarators:
                             self.generate_obfuscated_name(declarator.name)
-                    # elif isinstance(statement, javalang.tree.StatementExpression):
-                    #     # MethodInvocation의 변수나 메서드 식별자 처리
-                    #     if isinstance(statement.expression, javalang.tree.MethodInvocation):
-                    #         if statement.expression.qualifier:
-                    #             self.generate_obfuscated_name(statement.expression.qualifier)
-                    #         self.generate_obfuscated_name(statement.expression.member)
+                    elif isinstance(statement, javalang.tree.StatementExpression):
+                        # MethodInvocation의 변수나 메서드 식별자 처리
+                        if isinstance(statement.expression, javalang.tree.MethodInvocation):
+                            if statement.expression.qualifier:
+                                self.generate_obfuscated_name(statement.expression.qualifier)
+                            self.generate_obfuscated_name(statement.expression.member)
+
 
 
     def apply_obfuscation_to_files(self):
@@ -150,11 +163,7 @@ class ob_identifier:
 
         os.remove(file_path)
         # 난독화 맵을 사용하여 소스 코드에 난독화된 식별자 치환 적용
-        obfuscated_code = self.replace_identifiers_in_code(source_code)
-
-
-        relative_path = os.path.relpath(file_path, self.folder_path)
-        base_dir, original_filename = os.path.split(relative_path)
+        obfuscated_code = self.replace_identifiers_in_code(source_code,file_path)
 
         # 파일 이름을 난독화된 클래스 또는 Enum 이름으로 변경
         file_name = os.path.basename(file_path)
@@ -165,18 +174,45 @@ class ob_identifier:
 
         # 새 파일 이름으로 저장 (클래스나 Enum 이름에 맞춰 파일명 설정)
         new_file_name = f"{class_or_enum_obfuscated}.java"
-        output_path = os.path.join(self.output_folder, base_dir, new_file_name)
+        output_path = os.path.join(output_folder, new_file_name)
 
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
         with open(output_path, 'w', encoding='utf-8') as new_file:
             new_file.write(obfuscated_code)  # 난독화된 코드를 새 파일에 저장
 
-    def replace_identifiers_in_code(self, source_code):
+    def analyze_class_declaration(self,line): # 클래스 식별
+        pattern = r'''
+            ^\s*                    # 라인 시작과 앞쪽 공백
+            (public|private|protected)?\s* # 접근 제어자 (선택적)
+            (abstract|final)?\s*    # abstract 또는 final 키워드 (선택적)
+            class\s+                # class 키워드
+            (\w+)                   # 클래스 이름
+            (\s+extends\s+\w+)?     # 상속 (선택적)
+            (\s+implements\s+\w+(\s*,\s*\w+)*)?  # 인터페이스 구현 (선택적)
+            \s*(\{|$)               # 중괄호 시작 또는 라인 끝
+        '''
+        match = re.match(pattern, line, re.VERBOSE)
+
+        if match:
+            class_name = match.group(3)  # 클래스 이름은 세 번째 그룹
+            return True, class_name
+        else:
+            return False, None
+
+
+
+
+    def replace_identifiers_in_code(self, source_code,file_path):
         """난독화된 식별자 맵을 사용하여 소스 코드 내의 모든 식별자를 정확하게 치환하되, 리터럴 문자열은 제외."""
+
+        external_class = set()
+
+        imp_var_list = list()
 
         # 파일의 각 라인을 처리
         lines = source_code.splitlines()
+        curr_class =None
         for i, line in enumerate(lines):
             # import로 시작하는 라인을 처리
             if line.strip().startswith('import'):
@@ -185,12 +221,20 @@ class ob_identifier:
                     package_name = package_name_match.group(1)
 
                     # 패키지 맵에 있는 패키지이면 난독화 적용
-                    if not any(package_name.startswith(pkg) for pkg in self.package_map):
+                    if any(package_name.startswith(pkg) for pkg in self.package_map):
+                        pass
+                    else:
+                        external_class.add(package_name.split('.')[-1])
                         continue
 
 
-            # 복호화 코드 내 리터럴 문자 복호화
+            """ 현재 클래스 식별 """
+            is_class, class_name = self.analyze_class_declaration(line)
+            if is_class:
+                curr_class = class_name
+                imp_var_list = []
 
+            # 복호화 코드 내 리터럴 문자 복호화
             # Class.forName("클래스명") 패턴 찾기
             class_for_name_matches = re.finditer(r'Class\.forName\("([\w.]+)"\)', line)
             for match in class_for_name_matches:
@@ -208,22 +252,46 @@ class ob_identifier:
                     line = line.replace(method_name_literal, obfuscated_method_name)
 
 
+            # 함수 호출 패턴 (외부 함수 난독화에서 제외)
+            pattern = r'(\w+)\.(\w+)\(\)'
+            matches = re.findall(pattern, line)
+            for ii in range(len(matches)):
+                var , fun =  matches[ii]
+                if var in imp_var_list or var in external_class:
+                    line = line.replace(var+"."+fun+"(",var+"."+fun+"_DO_NOT_OBFUSCATE(")
+
 
             # 문자열 리터럴 ("...")을 분리하여 처리
             parts = re.split(r'(".*?")', line)  # 큰따옴표로 묶인 문자열은 분리
             for j, part in enumerate(parts):
                 # 문자열 리터럴은 그대로 두고, 리터럴이 아닌 코드 부분만 난독화 처리
                 if not part.startswith('"'):  # 큰따옴표로 시작하지 않으면 코드 부분
-                    for original, obfuscated in self.identifier_map.items():
-                        # 일반적인 식별자 패턴
-                        pattern = r'\b' + re.escape(original) + r'\b'
-                        part = re.sub(pattern, obfuscated, part)
-                # parts 리스트에 다시 수정된 코드를 반영
+
+
+
+                    """ 변수 선언 및 정의 식별 """
+                    variable_pattern = re.compile(r'(\w+)\s+(\w+)\s*=\s*') # 타입 변수 = 값 패턴을 찾는건데 타입 변수; 이렇게 끝나는 경우는?
+
+                    match = variable_pattern.search(part)
+                    if match:
+                        var_type, var_name = match.groups()
+                        if var_type in external_class:
+                            imp_var_list.append(var_name)
+
+
+
+                for original, obfuscated in self.identifier_map.items():
+
+                    # 일반적인 식별자 패턴
+                    pattern = r'\b' + re.escape(original) + r'\b'
+                    part = re.sub(pattern, obfuscated, part)
+
+
+                part = part.replace("_DO_NOT_OBFUSCATE","")
                 parts[j] = part
 
-            # 수정된 라인을 결합
-            lines[i] = ''.join(parts)
 
+            lines[i] = ''.join(parts)
         # 난독화된 코드를 반환
         obfuscated_code = '\n'.join(lines)
         return obfuscated_code
