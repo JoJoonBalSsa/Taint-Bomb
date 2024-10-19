3c2e91a6434116a10d9fb1caf69a9f49927f1948
Lanternbit <50915637+Lanternbit@users.noreply.github.com>
2024-10-19 15:48:36 +0900

    Update main.py

--- src/main/resources/pyscripts/main.py
+++ src/main/resources/pyscripts/main.py
@@ -76,7 +76,7 @@ def main(output_folder) :
         create_result(output_folder, tainted.flows)
         __analyze_method(output_folder, tainted)
 
-        make_md = MakeMD(output_folder + "/result.txt", output_folder + "/analysis_result.md")
+        make_md = MakeMD(output_folder + "/result.txt", output_folder + "/analysis_result.md", priority_flow)
         make_md.make_md_file()
 
 
